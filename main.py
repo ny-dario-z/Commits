2754bdf5566bb3ed527830eb0ad18333001d1beb
ny-lionel-s <lionel.schultze@noseryoung.ch>
2026-06-10 16:01:37 +0200

    made basic program

---
+++ main.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+
+counter = 0
+
+while True:
+    counter += 1
+
+    with open("counter.txt", "w") as f:
+        f.write(str(counter))
+
+    subprocess.run(["git", "add", "counter.txt"], check=True)
+    subprocess.run(
+        ["git", "commit", "-m", f"Auto commit #{counter}"],
+        check=True
+    )
+
+    # Optional: direkt zu GitHub pushen
+    # subprocess.run(["git", "push"], check=True)
+
+    time.sleep(10)
